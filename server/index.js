4bf13a3d29fb66ae8841d3b79851e5c263530c42
Teja Juthuga <tejajuthuga@gmail.com>
2024-04-02 10:54:19 +0530

    Developing User Registration server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
 const express = require('express');
-const mongoose = require("mongoose");
 
 const app = express();
 
-const dbConnection = mongoose.connect("mongodb+srv://teja_071199:test@atlascluster.a4vovbu.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
+const dbConnection = mongoose.connect("mongodb+srv://teja_071199:test@atlascluster.a4vovbu.mongodb.net/")
 
 dbConnection.then(() => {
     console.log("Database connection configured successfully!")
 })
 
 
-app.post("/RegisterUser", (req,res) => {
-    
-})
+app.use(express.json());
+
 
 
 app.listen(8081, () => {
