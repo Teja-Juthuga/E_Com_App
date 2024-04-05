f6641f3da4c71bcc4562d2ef286aeb89a6096f81
Teja Juthuga <tejajuthuga@gmail.com>
2024-04-05 14:28:31 +0530

    Hash

--- server/routes/login.route.js
+++ server/routes/login.route.js
@@ -1,40 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-const {connectDB} = require('../database/connectDB');
-
+const { connectDB } = require("../database/connectDB");
+const collectionName = "users";
 const main = async (userDetails) => {
-    const connection = await connectDB();
-    const collection = connection.collection("users");
-    const dbUser = await collection.findOne({emailId : userDetails.userEmail});
-    if (dbUser === undefined){
-        return "Invalid User";
-    }else {
-        const isPasswordMatched = await bcrypt.compare(userDetails.password, dbUser.password);
-        if (isPasswordMatched === true) {
-            return "Login Success!";
-          } else {
-            return "Invalid Password";
-          }
-    }
+    const db = await connectDB();
+    const collection = db.collection(collectionName);
+    let obj = { "email": userDetails.userEmail };
+    const dbUser = await collection.find(obj).toArray();
+    console.log(dbUser);    
+    return dbUser;
 };
 
 router.post("/", async (req, res) => {
-    try{
-        // console.log(req.body);
+    try {
         const result = await main(req.body);
-        console.log(result);
-        if (result){
-            res.status(400).send("Invaild Credentails");
-        }
-        else {
-            res.status(200).send("Authorised user")
-        }
-    }catch(err){
+        res.send(result);
+    } catch (err) {
+        console.log("+++Error:- " + err);
         res.send("Error: " + err);
     }
-})
+});
 
 module.exports = router;
